6b0d00e8b275e741aee6621c0d8089a8ec8fe139
Yash Sharma <55599815+YashSharma373@users.noreply.github.com>
2021-05-24 12:58:02 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,60 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { useState } from "react";
+import Slide from "./Slide";
+import "../styles/App.css";
 
-const App = () => {
+const App = ({ slides }) => {
+  const [first, setFirst] = useState(true);
+  const [last, setLast] = useState(false);
+  const [count, setCount] = useState(0);
+
+  // const length = slides.length;
+
+  function handleNextClick() {
+    setFirst(false);
+    setCount(count + 1);
+    if (count === 3) {
+      setLast(true);
+    }
+  }
+  function handlePrevious(){
+      setCount(count-1);
+      if(count===1){
+        setFirst(true);
+      }else if(count===4){
+        setLast(false);
+      }
+  }
+
+  function handleRestart() {
+    setCount(0);
+    setLast(false);
+    setFirst(true);
+  }
   return (
-    <></>
-  )
-}
-
+    <>
+      <Slide content={slides[count]} />
+      <button
+        onClick={handleRestart}
+        data-testid="button-restart"
+        disabled={first}
+      >
+        Restart
+      </button>
+      <button
+        onClick={handleNextClick}
+        data-testid="button-next"
+        disabled={last}
+      >
+        Next
+      </button>
+      <button 
+      onClick={handlePrevious}
+      data-testid="button-prev" disabled={first}>
+        Previous
+      </button>
+    </>
+  );
+};
 
 export default App;
+
